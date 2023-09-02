ae6381168a7652459453024afda2ab863adb6571
CarsonLeavitt <cdl016@uark.edu>
2023-09-02 11:38:18 -0500

    Basic calculations for the length of the hypotenuse

--- script1.py
+++ script1.py
@@ -1,1 +1,12 @@
-#Some python script that could do something.
+# Runs the Pythagorean theorem
+
+# Lengths of the sides
+a = 3
+b = 4
+
+# Calculate hypotenuse
+c_sq = a**2 + b**2
+c = c_sq**(1/2)
+
+# Output result
+print(f'c={c}')
