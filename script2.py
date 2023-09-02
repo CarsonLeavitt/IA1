0529ae6571d513de5b362db0471422d30309b126
CarsonLeavitt <cdl016@uark.edu>
2023-09-02 12:04:58 -0500

    Basic script to add values to a tally

--- script2.py
+++ script2.py
@@ -1,1 +1,21 @@
-#Another python script.
+# Script to add values to a tally.
+
+# initialize total
+total = 0
+
+# initialize value input
+value_in = '0'
+
+# initial instructions
+print('Type an amount to add or x to exit.')
+
+# loop until 'x' is entered
+while value_in != 'x':
+    # convert input to number and add onto total
+    value = float(value_in)
+    total = total + value
+
+    # output the new total and prompt for next input
+    print(f'Total={total}')
+    print("")
+    value_in = input('Add:')
